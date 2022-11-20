74d65c6da54455a9ce8872cbe0dfa246ba70301e
neo <zhanghan.7@foxmail.com>
2022-11-21 00:09:45 +0800

    enemy can fire

---
+++ Source/FPS_TestProject/LightSwitchCodeOnly.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LightSwitchCodeOnly.h"
+
+// Sets default values
+ALightSwitchCodeOnly::ALightSwitchCodeOnly()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALightSwitchCodeOnly::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALightSwitchCodeOnly::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
